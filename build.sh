e4a7d6553fbf02c1a53a1ae3e93d2a42d527971d
Jason Parham <parham@wildme.org>
2019-12-10 17:09:30 -0800

    Added build options

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # docker build --target org.wildme.ibeis.pytorch --tag wildme/pytorch:1.3-cuda10.1-cudnn7-magma100-devel-ubuntu18.04 .
-docker build --target org.wildme.ibeis.kaggle7.train --tag wildme/kaggle7:latest .
+# docker build --target org.wildme.ibeis.kaggle7.train --tag wildme/kaggle7:latest .
 docker build --target org.wildme.ibeis.kaggle7.server --tag wildme.azurecr.io/ibeis/kaggle7:latest .
